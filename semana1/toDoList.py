24893f5d1ace4a44f63a36aefe00331fe8f5595e
Ybrittoom <yagoemanuel99@gmail.com>
2025-05-12 17:14:22 -0300

    semana 1 finalizada dia 12/05/2025

--- semana1/toDoList.py
+++ semana1/toDoList.py
@@ -1,28 +1,35 @@
+import time
+
 def inicioToDoList():
     print('Bem-Vindo ao ToDoList mais top do mundo!!')
-    print('Escolha uma funçao:\n ' 
-    '1 - Adicionar Tarefa\n ' 
-    '2 - Listar Tarefas\n ' 
-    '3 - Remover Tarefa\n ' 
-    '4 - Concluir Tarefa\n ' 
-    '5 - Sair ')
+    while True: #loop principal para nao ecerrar o programa enquanto o usuario nao quiser sair
+        print('Escolha uma funçao:\n ' 
+        '1 - Adicionar Tarefa\n ' 
+        '2 - Listar Tarefas\n ' 
+        '3 - Remover Tarefa\n ' 
+        '4 - Concluir Tarefa\n ' 
+        '5 - Sair ')
 
-    funcaoDaLista = int(input('Escolha uma funçao(numero): '))
+        try:
+            funcaoDaLista = int(input('Escolha uma funçao(numero): '))
+            if (funcaoDaLista == 1):
+                adicionarTarefa()
+            elif (funcaoDaLista == 2):
+                listarTarefas()
+            elif (funcaoDaLista == 3):
+                removerTarefa()
+            elif (funcaoDaLista == 4):
+                concluirTarefa()
+            elif (funcaoDaLista == 5):
+                print('Saindo do ToDoList. Até a próxima!')
+                break #sai do loop principal e encerra o programa
+            else:
+                print('escolha um funçao valida')
+        except ValueError:
+            print('Por favor, digite um número para escolher a função.')
+        time.sleep(2)
 
-    if (funcaoDaLista == 1):
-        adicionarTarefa()
-    elif (funcaoDaLista == 2):
-        listarTarefas()
-    elif (funcaoDaLista == 3):
-        removerTarefa()
-    elif (funcaoDaLista == 4):
-        concluirTarefa()
-    elif (funcaoDaLista == 5):
-        sair()
-    else:
-        print('escolha um funçao valida')
-
-tarefas = ['cavalo', 'boi']
+tarefas = []
 
 def adicionarTarefa():
     novaTarefa = input('Digite a nova tarefa: ')
@@ -39,13 +46,37 @@ def listarTarefas():
 
 
 def removerTarefa():
-    tarefaRemover = input('Digite a tarefa que deseja remover: ')
-    if tarefaRemover in tarefas:
-        tarefas.remove(tarefaRemover)
-        print('Tarefa removida com sucesso!')
+    listarTarefas()
+    entrada = input('Digite (nome ou numero) da tarefa que deseja remover: ')
+
+    try:
+        indice = int(entrada)
+        if 0 <= indice < len(tarefas):
+            tarefaRemovida = tarefas.pop(indice)
+            print(f'{tarefaRemovida} removida com sucesso!')
+            print('Lista de Tarefas: ')
+            print(tarefas)
+        else:
+            print('Tarefa nao encontrada!!')
+            removerTarefa()
+    except ValueError:
+        if entrada in tarefas:
+            tarefas.remove(entrada)
+            print('Tarefa removida com sucesso!')
+            print('Lista de Tarefas: ')
+            print(tarefas)
+        else:
+            print(f'Tarefa com o nome {entrada} nao encontrada')
+            removerTarefa()
+
+def concluirTarefa():
+    listarTarefas()
+    tarefaConcluida = input('Digite a tarefa que deseja concluir: ')
+    if tarefaConcluida in tarefas:
+        tarefas.remove(tarefaConcluida)
+        print('Tarefa concluida com sucesso!')
     else:
         print('Tarefa nao encontrada')
 
 inicioToDoList()
-print(tarefas[1])
-print(tarefas)
+inicioToDoList()
